34b3862e0998fcee3c93fb64c1a4bea3c6ebd6ef
pedrounivates5221 <pedro.gregory@universo.univates.br>
2025-04-24 18:20:09 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+function alerta(){
+
+    alert("curiosidade jogos agora estão custando 90 dolares");
+}
+
+
+
+
+var contador = 0;
+         var contador = 0;
+          function like(){
+         contador++;
+          document.getElementById("like2").innerHTML =contador;
+          }
